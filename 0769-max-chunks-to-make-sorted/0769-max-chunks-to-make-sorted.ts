5a454551e89f28bc81a21ea6842036010ce09628
Ashok <36642819+ashoksethy@users.noreply.github.com>
2024-12-19 23:45:36 +0530

    Time: 0 ms (100.00%), Space: 51.7 MB (14.29%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.ts
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.ts
@@ -16,4 +16,4 @@ function maxChunksToSorted(arr: number[]): number {
 // Test cases 
 console.log(maxChunksToSorted([4, 3, 2, 1, 0])); // Output: 1
 console.log(maxChunksToSorted([1, 0, 2, 3, 4])); // Output: 4
-
+console.log(maxChunksToSorted([2, 0, 1]));       // Output: 1
