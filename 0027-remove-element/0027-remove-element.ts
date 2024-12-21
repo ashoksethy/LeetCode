b3a45ba8ef6b11bcba1869f9e5e3317a54eb4ba3
Ashok <36642819+ashoksethy@users.noreply.github.com>
2024-12-21 23:15:27 +0530

    Time: 0 ms (100.00%), Space: 51.9 MB (12.76%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -3,23 +3,24 @@ function removeElement(nums: number[], val: number): number {
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[k] = nums[i];
-            k++;
+            nums[k] = nums[i]; // Move the current element to position k
+            k++; // Increment k
         }
     }
 
-    return k;
+    return k; // k is the count of elements not equal to val
 }
+
 
 // Test cases
 const nums1 = [3, 2, 2, 3];
 const val1 = 3;
 const k1 = removeElement(nums1, val1);
-console.log(k1); 
-console.log(nums1.slice(0, k1));
+console.log(k1); // Output: 2
+console.log(nums1.slice(0, k1)); // Output: [2, 2]
 
 const nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
 const val2 = 2;
 const k2 = removeElement(nums2, val2);
-console.log(k2); 
-console.log(nums2.slice(0, k2));
+console.log(k2); // Output: 5
+console.log(nums2.slice(0, k2)); // Output: [0, 1, 3, 0, 4]
